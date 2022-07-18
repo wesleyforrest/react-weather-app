b2aaeabbb92e44badcdbe9d475473799f48a88b5
wesleyforrest <79322205+wesleyforrest@users.noreply.github.com>
2022-07-18 12:15:38 +0100

    Start of header design

---
+++ src/Containers/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.scss";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1 className="header__title">Wes's Wonderful Weather</h1>
+      <p className="header__description">
+        The best app on the WWW for all your maritime leasure pursuits!
+      </p>
+    </div>
+  );
+};
+
+export default Header;
