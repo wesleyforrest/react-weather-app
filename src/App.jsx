e8b05614290922d845ba1c4c05adebf16f1ca409
wesleyforrest <79322205+wesleyforrest@users.noreply.github.com>
2022-07-19 12:47:58 +0100

    Api call semi working

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./Containers/Header/Header";
 import LocationMap from "./Containers/LocationMap/LocationMap";
+import WeatherData from "./Components/WeatherData/WeatherData";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <LocationMap />
+      <WeatherData />
     </div>
   );
 }
--- src/Components/WeatherData/WeatherData.jsx
+++ src/Components/WeatherData/WeatherData.jsx
@@ -1,25 +1,35 @@
 import React from "react";
+import { useEffect } from "react";
 
 const WeatherData = () => {
-  const lat = 58.7984;
-  const lng = 17.8081;
-  const params = "windSpeed";
+  const GetTides = () => {
+    const lat = 58.7984;
+    const lng = 17.8081;
+    const params = "windSpeed";
 
-  fetch(
-    `https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${params}`,
-    {
-      headers: {
-        Authorization:
-          "813f21e2-0745-11ed-b6bf-0242ac130002-813f2282-0745-11ed-b6bf-0242ac130002",
-      },
-    }
-  )
-    .then((response) => response.json())
-    .then((jsonData) => {
-      // Do something with response data.
+    fetch(
+      `https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${params}`,
+      {
+        headers: {
+          Authorization:
+            "813f21e2-0745-11ed-b6bf-0242ac130002-813f2282-0745-11ed-b6bf-0242ac130002",
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((jsonData) => {
+        console.log(jsonData);
+        // Do something with response data.
+      });
+
+    useEffect(() => {
+      GetTides();
     });
 
-  return <div>WeatherData</div>;
+    console.log(WeatherData);
+
+    return <div>WeatherData</div>;
+  };
 };
 
 export default WeatherData;
