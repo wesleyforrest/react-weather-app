af742073e238c7c2db4024aec70731f0371bb989
wesleyforrest <79322205+wesleyforrest@users.noreply.github.com>
2022-07-20 13:01:45 +0100

    Added placeholder map and added geo location

--- src/Components/LocationService/LocationService.jsx
+++ src/Components/LocationService/LocationService.jsx
@@ -1,23 +1,46 @@
 import React from "react";
+import { useGeolocated } from "react-geolocated";
 
 const LocationService = () => {
-  const getLocation = () => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-      alert("Can't find location");
-    }
-  };
+  const { coords, isGeolocationAvailable, isGeolocationEnabled } =
+    useGeolocated({
+      positionOptions: {
+        enableHighAccuracy: false,
+      },
+      userDecisionTimeout: 5000,
+    });
 
-  const showPosition = (position) => {
-    const myLatitude = showPosition;
-  };
-  getLocation();
-
-  return (
-    <div>
-      <h2>Your current location is{getLocation}</h2>
-    </div>
+  return !isGeolocationAvailable ? (
+    <div>Your browser does not support Geolocation</div>
+  ) : !isGeolocationEnabled ? (
+    <div>Geolocation is not enabled</div>
+  ) : coords ? (
+    <table>
+      <tbody>
+        <tr>
+          <td>latitude</td>
+          <td>{coords.latitude}</td>
+        </tr>
+        <tr>
+          <td>longitude</td>
+          <td>{coords.longitude}</td>
+        </tr>
+        <tr>
+          <td>altitude</td>
+          <td>{coords.altitude}</td>
+        </tr>
+        <tr>
+          <td>heading</td>
+          <td>{coords.heading}</td>
+        </tr>
+        <tr>
+          <td>speed</td>
+          <td>{coords.speed}</td>
+        </tr>
+      </tbody>
+    </table>
+  ) : (
+    <div>Getting the location data&hellip; </div>
   );
 };
 
