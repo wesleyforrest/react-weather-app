6bf38115e5b673b137f8e30d001a64295301d3da
wesleyforrest <79322205+wesleyforrest@users.noreply.github.com>
2022-07-28 16:04:20 +0100

    Added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import "./App.css";
 import Header from "./Containers/Header/Header";
-import LocationMap from "./Containers/LocationMap/LocationMap";
-import WeatherData from "./Components/WeatherData/WeatherData";
-import LocationService from "./Components/LocationService/LocationService";
-import LocationCard from "./Containers/LocationCard/LocationCard";
 import SailingLocations from "./Containers/SailingLocations/SailingLocations";
 import CurrentLocation from "./Components/CurrentLocation/CurrentLocation";
+import Footer from "./Containers/Footer/Footer";
 
 function App() {
   return (
@@ -13,6 +10,7 @@ function App() {
       <Header />
       <SailingLocations />
       <CurrentLocation />
+      <Footer />
     </div>
   );
 }
---
+++ src/Containers/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Footer.scss;";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <h3>Wesley Forrest</h3>
+      <h4>nology 12 week full stack bootcamp</h4>
+      <h3>2022</h3>
+    </div>
+  );
+};
+
+export default Footer;
