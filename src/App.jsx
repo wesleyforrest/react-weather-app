61e0b47fc1922b5a352fe70dec2e682036ab671c
wesleyforrest <79322205+wesleyforrest@users.noreply.github.com>
2022-08-10 11:31:46 +0100

    Styled current location

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import "./App.css";
 import Header from "./Containers/Header/Header";
 import SailingLocations from "./Containers/SailingLocations/SailingLocations";
 import CurrentLocation from "./Components/CurrentLocation/CurrentLocation";
+import LocationCard from "./Containers/LocationCard/LocationCard";
 import Footer from "./Containers/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <SailingLocations />
       <CurrentLocation />
+      <SailingLocations />
       <Footer />
     </div>
   );
