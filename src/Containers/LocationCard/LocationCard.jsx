73487ed33b2215cc9845074b0bd5d588d0fbbc1b
wesleyforrest <79322205+wesleyforrest@users.noreply.github.com>
2022-08-15 12:52:01 +0100

    Changed location class to "flip-card", cards flip

--- src/Containers/LocationCard/LocationCard.jsx
+++ src/Containers/LocationCard/LocationCard.jsx
@@ -8,15 +8,33 @@ const LocationCard = (props) => {
 
   console.log(marina);
   return (
-    <div className="location-card">
-      <h1 className="location__marina">{marina.marina}</h1>
-      <img className="location-card__image" src={marina.image} alt="else" />
-      <h4 className="location__longitude">Longitude: {marina.longitude}</h4>
-      <h4 className="location__latitude">Latitude: {marina.latitude}</h4>
-      <h4 className="location__vhf">VHF Channel: {marina.vhf}</h4>
-      <div className="location-Card__buttons">
-        <button className="location-Card__buttons__weather">WeatherInfo</button>
-        <button className="location-Card__buttons__tides">Tide Info</button>
+    <div className="flip-card">
+      <div className="flip-card-inner">
+        <div className="flip-card-front">
+          <h1 className="flip-card__marina">{marina.marina}</h1>
+          <img
+            className="flip-card-front__image"
+            src={marina.image}
+            alt="else"
+          />
+          <h4 className="flip-card__longitude">
+            Longitude: {marina.longitude}
+          </h4>
+          <h4 className="flip-card__latitude">Latitude: {marina.latitude}</h4>
+          <h4 className="flip-card__vhf">VHF Channel: {marina.vhf}</h4>
+          <div className="flip-card-Card__buttons">
+            <button className="flip-card-card__buttons-weather">
+              WeatherInfo
+            </button>
+            <button className="flip-card-card__buttons-tides">Tide Info</button>
+          </div>
+        </div>
+        <div className="flip-card-weather">
+          <h2>Weather Info</h2>
+        </div>
+        <div className="flip-card-tide">
+          <h2>Tide info</h2>
+        </div>
       </div>
     </div>
   );
